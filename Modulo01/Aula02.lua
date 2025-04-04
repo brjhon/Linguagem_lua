6f7902d4771c29b0513d0e6312cde81103cd9642
jhonwesley <102744615+brjhon@users.noreply.github.com>
2025-04-04 18:39:55 -0300

    Aula02.lua

---
+++ Modulo01/Aula02.lua
@@ -0,0 +1,14 @@
+--[[
+local Valor1 = "Ola, mundo!"
+local Valor2 = "Outra informacao"
+print(Valor1)
+
+local Valor = "Ola, mundo!" -- Nota 
+print(Valor)
+
+local Valor = 20 --Number
+print(Valor)
+]]
+
+local Valor = true/false -- boolean 
+print(Valor)
