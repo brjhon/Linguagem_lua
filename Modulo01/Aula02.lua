7bb1f22b5ac2c89ea6956bd84cf85b56d85b8a60
jhonwesley <102744615+brjhon@users.noreply.github.com>
2025-04-08 08:55:37 -0300

    upgrad

--- Modulo01/Aula02.lua
+++ Modulo01/Aula02.lua
@@ -8,7 +8,15 @@ print(Valor)
 
 local Valor = 20 --Number
 print(Valor)
-]]
 
 local Valor = true/false -- boolean 
 print(Valor)
+Olimpiadas@2019
+
+local n1 = 20 -- Number
+print(type(n1)) --Saída: Number
+--]]
+
+local valor ="20"
+local tipo = type(valor) -- tipo é uma string
+print(tipo) -- Saída: string
